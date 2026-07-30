e023a0584431525abbe7a418ec861dae41772245
Maxim Maslii <140587592+MaximMaslii@users.noreply.github.com>
2026-07-30 16:46:49 +0200

    Update user_repository.py

--- backend/app/repositories/user_repository.py
+++ backend/app/repositories/user_repository.py
@@ -19,17 +19,25 @@ class UserRepository:
 
     @staticmethod
     async def create(document: dict):
-        await db.users.insert_one(document)
+        return await db.users.insert_one(document)
 
     @staticmethod
     async def update(user_id: str, values: dict):
-        await db.users.update_one(
+        return await db.users.update_one(
             {"user_id": user_id},
             {"$set": values},
         )
 
     @staticmethod
     async def delete(user_id: str):
-        await db.users.delete_one(
+        return await db.users.delete_one(
             {"user_id": user_id},
         )
+
+    @staticmethod
+    async def email_exists(email: str):
+        user = await db.users.find_one(
+            {"email": email},
+            {"_id": 1},
+        )
+        return user is not None
