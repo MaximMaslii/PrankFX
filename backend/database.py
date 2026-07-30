b86dbb3933fe15e51ec449e6c894c2b46e90be97
Maxim Maslii <140587592+MaximMaslii@users.noreply.github.com>
2026-07-30 14:59:27 +0200

    Create database.py

---
+++ backend/database.py
@@ -0,0 +1,13 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from app.config import settings
+
+client = AsyncIOMotorClient(settings.MONGO_URL)
+
+db = client[settings.DB_NAME]
+
+def get_db():
+    return db
+
+async def close_database():
+    client.close()
