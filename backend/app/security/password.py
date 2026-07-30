48332b39ce2fd593c82b9ecb482a85ac93c70841
Maxim Maslii <140587592+MaximMaslii@users.noreply.github.com>
2026-07-30 16:59:16 +0200

    Update password.py

--- backend/app/security/password.py
+++ backend/app/security/password.py
@@ -1,3 +1,14 @@
-hash_password
+from passlib.context import CryptContext
 
-verify_password
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto",
+)
+
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(password: str, password_hash: str) -> bool:
+    return pwd_context.verify(password, password_hash)
