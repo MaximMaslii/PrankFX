cf71fe13ae4f709ea9e389d1f2cf50554adfe3c0
Maxim Maslii <140587592+MaximMaslii@users.noreply.github.com>
2026-07-30 14:57:11 +0200

    Create config.py

---
+++ backend/config.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+
+class Settings(BaseSettings):
+
+    MONGO_URL: str
+
+    DB_NAME: str
+
+    JWT_SECRET: str
+
+    JWT_ALGORITHM: str = "HS256"
+
+    EMERGENT_LLM_KEY: str = ""
+
+    class Config:
+        env_file = ROOT_DIR / ".env"
+
+settings = Settings()
