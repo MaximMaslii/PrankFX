a87d0c6247edec19d76d580b2f4e73bbe42512b6
Maxim Maslii <140587592+MaximMaslii@users.noreply.github.com>
2026-07-30 16:54:17 +0200

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,13 +1,16 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+
 from app.config import settings
-from app.database import db
+
 
 client = AsyncIOMotorClient(settings.MONGO_URL)
 
 db = client[settings.DB_NAME]
 
+
 def get_db():
     return db
+
 
 async def close_database():
     client.close()
