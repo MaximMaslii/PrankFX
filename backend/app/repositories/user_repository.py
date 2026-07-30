a927a46342328900524a1ca4cdc2ee6448bbabaa
Maxim Maslii <140587592+MaximMaslii@users.noreply.github.com>
2026-07-30 16:34:58 +0200

    Update user_repository.py

--- backend/app/repositories/user_repository.py
+++ backend/app/repositories/user_repository.py
@@ -1,1 +1,28 @@
+from app.database import db
 
+
+class UserRepository:
+
+    @staticmethod
+    async def get_by_email(email: str):
+        return await db.users.find_one(
+            {"email": email},
+            {"_id": 0}
+        )
+
+    @staticmethod
+    async def get_by_user_id(user_id: str):
+        return await db.users.find_one(
+            {"user_id": user_id},
+            {"_id": 0}
+        )
+
+    @staticmethod
+    async def create(user: dict):
+        return await db.users.insert_one(user)
+
+    @staticmethod
+    async def delete(user_id: str):
+        await db.users.delete_one(
+            {"user_id": user_id}
+        )
